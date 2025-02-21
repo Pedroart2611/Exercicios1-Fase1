6a0e29f0a07f759b1f79a2d18235fcfcaa25302b
Pedroart2611 <pedro.murillo@soulasalle.com.br>
2025-02-20 21:41:31 -0300

    Add files via upload

---
+++ Exercicio1_Fase1.java
@@ -0,0 +1,118 @@
+package bundus;
+
+import java.util.Scanner;
+
+public class Exercicio1_Fase1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //1
+        
+        /*System.out.print("Escreva um nuemero: ");
+        int n1 = sc.nextInt();
+        System.out.print("Escreva um segundo numero: ");
+        int n2 = sc.nextInt();
+        
+        int total = n1 + n2;
+        
+        System.out.print("A soma é: " +total);*/
+        
+        //2
+        
+       /* System.out.print("Digite o  raio do circulo:");
+        double  raio = sc.nextInt();
+        
+        double area  = 3.14159 * (raio * raio);
+        
+        System.out.print("A área do circulo é: "  + area);*/
+        
+        //3
+        
+        /*System.out.print("Digite a temperatura em celcius");
+        double  temp  = sc.nextInt();
+        
+        double f = (temp * 9/5) + 32;
+        System.out.print("A temperatura em Fahrenheit: "  + f);*/
+        
+        //4
+        
+        /*System.out.print("Digite sua nota: ");
+        double n1 = sc.nextInt();
+        System.out.print("Digite outra nota: ");
+        double n2 =  sc.nextInt();
+        System.out.print("Digite sua ultima nota: ");
+        double n3 = sc.nextInt();
+        
+        double media = (n1 + n2 + n3) / 3;
+        
+        System.out.print("A media é: " + media);*/
+        
+        
+        
+        //5
+        
+       /*System.out.print("Digite sua idade: ");
+       int idade = sc.nextInt();
+        
+       int convert = idade * 365;
+       System.out.print("Sua idade em dias aproximadamente é: " + convert);*/
+        
+        //6
+        
+        /*System.out.print("Digite seu salario: ");
+        float salario = sc.nextFloat();
+        System.out.print("Digite o desconto do INSS: ");
+        float inss = sc.nextFloat();
+        
+        float liquido = salario - inss;
+        System.out.print("O seu salario liquido é: " + liquido);*/
+        
+        //7
+        
+        /*System.out.print("Digite a base do retangulo: ");
+        int base = sc.nextInt();
+        System.out.print("Agora digite a altura do retangulo: ");
+        int altura = sc.nextInt();
+        
+        int area = base * altura;
+        int parametro = (base * altura) * 2;
+        
+        System.out.print("A area é: " + area);
+        System.out.print("O parametro: " + parametro);*/
+        
+        //8
+        
+        /*System.out.print("Digite o raio da esfera: ");
+        double raio = sc.nextInt();
+        
+        double V = (4/3) * 3.14159 * (raio * raio * raio);
+        System.out.print("O volume da esfera é: " + V);*/
+        
+        //9
+        
+       /* System.out.print("Digite o valor em real: ");
+        double real = sc.nextInt();
+        
+        double dolar = real * 5.70;
+        System.out.print("O valor em dolar: " + dolar);*/
+        
+        //10
+        
+        System.out.print("Digite o valor de x1: ");
+        double x1 = sc.nextInt();
+        System.out.print("Digite o valor de y1: ");
+        double y1 = sc.nextInt();
+        System.out.print("Digite o valor de x2: ");
+        double x2 = sc.nextInt();
+        System.out.print("Digite o valor de y2:");
+        double y2 = sc.nextInt();
+        
+        double form = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2  - y1));
+        System.out.print("A distancia é: " + form);
+        
+        
+        
+        
+    }
+}
+
